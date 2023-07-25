9fcdeff64ffba7dac626bef1470526611bf3d70e
Laila <lailatabourit@gmail.com>
2023-07-25 03:46:59 -0700

    ALX_tasks

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,55 +1,53 @@
 #include "sort.h"
 
 /**
-* _swap - function that swaps two elements
-* @ar : array
-* @e1 : first element
-* @e2 : second element
-* @ord : 1 in ascending order, 0 descending
-* by: laila and mega
-*/
-void _swap(int ar, int e1, int e2, int ord)
-{
-	int tmp;
-
-	if (ord == (ar[e1] > ar[e2]))
-	{
-		tmp = ar[e1];
-		ar[e1] = ar[e2];
-		ar[e2] = tmp;
-	}
-}
-/**
- * _merge - function that sorts bitonic sequences recursively
- * in both orders
- * @ar: array
- * @low: first element
- * @nelement: number
- * @ord: 1 ascending order, 0 descending
- * by :laila and mega
- */
-void _merge(int ar[], int low, int nelement, int ord)
-{
-	int midd, n;
-
-	if (nelement > 1)
-	{
-		midd = nelement / 2;
-		for (n = low; n < low + midd; n++)
-			_swap(ar, n, n + midd, ord);
-		_merge(ar, low, midd, ord);
-		_merge(ar, low + midd, midd, ord);
-	}
-}
-/**
- * bitonicsort - function that bitonic sorts algorithm
- * implementation
+ * swap - change two values in ascending or descending order
  * @arr: array
- * @low: first elmt
- * @nelemnt: number of elements
- * @order: 1 ascending order, 0 descending order
- * @size: array lenght
+ * @item1: item one
+ * @item2: item two
+ * @order: 1: ascending order, 0 descending order
  * by : laila and mega
+ */
+void swap(int arr[], int item1, int item2, int order)
+{
+	int temp;
+
+	if (order == (arr[item1] > arr[item2]))
+	{
+		temp = arr[item1];
+		arr[item1] = arr[item2];
+		arr[item2] = temp;
+	}
+}
+/**
+ * merge - sorts bitonic sequences recursively in both orders
+ * @arr: array
+ * @low: first element
+ * @nelemnt: elements number
+ * @order: 1: ascending order, 0 descending order
+ * by: laila and mega
+ */
+void merge(int arr[], int low, int nelemnt, int order)
+{
+	int mid, i;
+
+	if (nelemnt > 1)
+	{
+		mid = nelemnt / 2;
+		for (i = low; i < low + mid; i++)
+			swap(arr, i, i + mid, order);
+		merge(arr, low, mid, order);
+		merge(arr, low + mid, mid, order);
+	}
+}
+/**
+ * bitonicsort - function bitonic sort algorithm implementation
+ * @arr: array
+ * @low: first element
+ * @nelemnt: number of elements
+ * @order: 1: ascending order, 0 descending order
+ * @size: array lenght
+ * by: laila and mega
  */
 void bitonicsort(int arr[], int low, int nelemnt, int order, int size)
 {
@@ -70,7 +68,7 @@ void bitonicsort(int arr[], int low, int nelemnt, int order, int size)
 		mid = nelemnt / 2;
 		bitonicsort(arr, low, mid, 1, size);
 		bitonicsort(arr, low + mid, mid, 0, size);
-		_merge(arr, low, nelemnt, order);
+		merge(arr, low, nelemnt, order);
 		if (order <= 0)
 		{
 			printf("Result [%i/%i] (DOWN):\n", nelemnt, size);
@@ -84,7 +82,7 @@ void bitonicsort(int arr[], int low, int nelemnt, int order, int size)
 	}
 }
 /**
- * bitonic_sort - function  prepare the terrain to bitonic sort algorithm
+ * bitonic_sort - prepare the terrain to bitonic sort algorithm
  * @array: array
  * @size: array lenght
  * by : laila and mega
